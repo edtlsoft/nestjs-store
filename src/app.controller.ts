7cf05919a79d4b4a7eb39c5f540cd9268c581352
Edward Lopez <edward.trigos@nivelics.co>
2021-12-27 11:33:55 -0500

    [add] Getting parameters from URL

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/products/:id/categories/:categoryId')
+  newEndpoint(
+    @Param('id') productId: string,
+    @Param('categoryId') categoryId: string,
+  ): string {
+    return `The product id is: ${productId} <br> And the category Id is: ${categoryId}`;
   }
 }
