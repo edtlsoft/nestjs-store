8bb8f1dd3d5c5e92536e3685b4f5c0e8cb0923d7
Edward Lopez <edward.trigos@nivelics.co>
2021-12-28 15:10:20 -0500

    [add] Use value

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
 
+const API_KEY = '123456';
+const API_KEY_PROD = '123456-secure';
+
 @Module({
   imports: [UsersModule, ProductsModule, PaymentsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
