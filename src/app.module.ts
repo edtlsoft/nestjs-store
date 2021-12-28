de1afb8ebd9d9531ead8f35adacd97e829847204
Edward Lopez <edward.trigos@nivelics.co>
2021-12-28 15:35:04 -0500

    [add] Use factory

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 const API_KEY = '123456';
 const API_KEY_PROD = '123456-secure';
 
 @Module({
-  imports: [UsersModule, ProductsModule, PaymentsModule],
+  imports: [UsersModule, ProductsModule, PaymentsModule, HttpModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await lastValueFrom(
+          http.get('https://jsonplaceholder.typicode.com/todos'),
+        );
+
+        return tasks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
