a5430cde860d483d4bb866fa218057fc02c5ef84
Edward Lopez <edward.trigos@nivelics.co>
2021-12-28 23:23:56 -0500

    [add] Integrating Swagger and PartialType with Open API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJs Store')
+    .setDescription('NestJs course')
+    .setVersion('1.0')
+    .addTag('nest')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
