2cbf78e2e57bef06ec6f0853fe3b4f26c9fbf114
Edward Lopez <edward.trigos@nivelics.co>
2021-12-30 12:11:37 -0500

    [add] TypeORM

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,12 +2,32 @@ import { Module, Global } from '@nestjs/common';
 import { Client } from 'pg';
 import { ConfigType } from '@nestjs/config';
 import config from '../config/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const API_KEY = '123456';
 const API_KEY_PROD = '123456-secure';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, name, password, port } = configService.database;
+
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: name,
+          entities: [],
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
@@ -32,6 +52,6 @@ const API_KEY_PROD = '123456-secure';
       inject: [config.KEY],
     },
   ],
-  exports: ['API_KEY', 'PG_CLIENT'],
+  exports: ['API_KEY', 'PG_CLIENT', TypeOrmModule],
 })
 export class DatabaseModule {}
