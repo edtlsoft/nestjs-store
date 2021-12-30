ba98c3867102bbac4f83bc66adc987bd547cbeb0
Edward Lopez <edward.trigos@nivelics.co>
2021-12-30 13:47:35 -0500

    [add] First entity with TypeORM

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,16 @@
-import { ProductsService } from './services/products/products.service';
-import { CategoriesService } from './services/categories/categories.service';
-import { BrandsService } from './services/brands/brands.service';
-import { ProductsController } from './controllers/products/products.controller';
-import { CategoriesController } from './controllers/categories/categories.controller';
-import { BrandsController } from './controllers/brands/brands.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
+import { ProductsController } from './controllers/products/products.controller';
+import { ProductsService } from './services/products/products.service';
+import { Product } from './entities/product.entity';
+import { CategoriesService } from './services/categories/categories.service';
+import { CategoriesController } from './controllers/categories/categories.controller';
+import { BrandsService } from './services/brands/brands.service';
+import { BrandsController } from './controllers/brands/brands.controller';
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [BrandsController, CategoriesController, ProductsController],
   providers: [BrandsService, CategoriesService, ProductsService],
   exports: [ProductsService],
