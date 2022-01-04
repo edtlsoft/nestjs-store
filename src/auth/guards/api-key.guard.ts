8e0d391cfba94f8bae07501cb60ba973cb3a0726
Edward Lopez <edward.trigos@nivelics.co>
2022-01-04 12:27:29 -0500

    [refactor] Guard with environment variables

--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -1,17 +1,23 @@
+import { ConfigType } from '@nestjs/config';
 import { IS_PUBLIC_KEY } from './../decorators/public.decorator';
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+import config from '../../config/config';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(
+    private reflector: Reflector,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
 
   canActivate(
     context: ExecutionContext,
@@ -23,7 +29,7 @@ export class ApiKeyGuard implements CanActivate {
 
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('Auth');
-    const isAuth = authHeader === 'auth1234';
+    const isAuth = authHeader === this.configService.apiKey;
 
     if (!isAuth) {
       throw new UnauthorizedException('Fuck! motherfucker!');
--- src/config/configSchema.ts
+++ src/config/configSchema.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export default Joi.object({
-  API_KEY: Joi.number().required(),
+  API_KEY: Joi.string().required(),
   DATABASE_HOST: Joi.string().required(),
   DATABASE_USER: Joi.string().required(),
   DATABASE_PASSWORD: Joi.string().required(),
